cacdcfb35295b4f1a5a79634ca13798a4fa8fb2b
Hieu Nguyen <hieunm.citynow@gmail.com>
2021-03-25 08:32:58 +0700

    Update Base API url

---
+++ src/constants/appConfig.js
@@ -0,0 +1,5 @@
+export const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
+
+export const VERSION = process.env.REACT_APP_VERSION;
+
+export const API_TIMEOUT = process.env.REACT_APP_API_TIMEOUT;
